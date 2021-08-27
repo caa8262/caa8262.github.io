d4903bfaf993be5609a404c490f345059aa29930
caa8262 <caa8262@ppsbstudent.org>
2021-08-27 19:06:13 +0000

    draft

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -35,10 +35,11 @@
          * bottom of the game.
          */
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
-
+        createPlatform()
         // example:
         createPlatform(400, 250);
         createPlatform(170, 460, 0.5);
+        createPlatform()
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
